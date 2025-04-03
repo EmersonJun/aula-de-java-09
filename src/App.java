e4ef92a00085275281a59435bb07f337325b4ee6
Emerson <emersonnjunior2006@gmail.com>
2025-04-03 20:08:14 -0300

    set

--- src/App.java
+++ src/App.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -67,16 +68,32 @@ public class App {
         set.add("css");
 
         System.out.println(set);
-        //linkedhashset nao mantem ordem nenhuma
+
+        
+        //linkedhashset mantem ordem
         Set<String> linkedSet = new LinkedHashSet<>();
-        set.add("java");
-        set.add("php");
-        set.add("php");
-        set.add("c++");
-        set.add("HTML5");
-        set.add("HTML5");
-        set.add("css");
+        linkedSet.add("java");
+        linkedSet.add("php");
+        linkedSet.add("php");
+        linkedSet.add("c++");
+        linkedSet.add("HTML5");
+        linkedSet.add("HTML5");
+        linkedSet.add("css");
 
         System.out.println(linkedSet);
+
+
+        //ordem alfabetica
+        Set<String> treedSet = new TreeSet<>();
+        treedSet.add("java");
+        treedSet.add("php");
+        treedSet.add("php");
+        treedSet.add("c++");
+        treedSet.add("HTML5");
+        treedSet.add("HTML5");
+        treedSet.add("css");
+
+        System.out.println(treedSet);
+
     }
 }
