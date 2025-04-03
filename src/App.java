3d9a2ff8045dfff39223a9b45211fe69a62b1148
Emerson <emersonnjunior2006@gmail.com>
2025-04-03 20:05:47 -0300

    set

--- src/App.java
+++ src/App.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -34,9 +37,46 @@ public class App {
             .findFirst().orElse(null);
         System.out.println(html);
 
+        aluno.addMateria(new Materia("php"));
         List<Materia> listaNova = aluno.getMaterias()
         .stream()
         .map(m -> new Materia(m.getNome() + "!")).collect(Collectors.toList());
         System.out.println(listaNova);
+
+        listaNova = aluno.getMaterias()
+        .stream().filter(m-> m.getNome().equals("Php")).collect(Collectors.toList());
+
+        System.out.println("lista nova: "+listaNova);
+
+        System.out.println(aluno.getMaterias()
+        .stream()
+        .map(Materia::getNome)
+        .reduce("\n Matrias: ", (novaString, stringInteradora) -> stringInteradora + "")
+        );
+
+
+        System.out.println("\n############# Set em java ##############\n");
+        //hashset nao mantem ordem nenhuma
+        Set<String> set = new HashSet<>();
+        set.add("java");
+        set.add("php");
+        set.add("php");
+        set.add("c++");
+        set.add("HTML5");
+        set.add("HTML5");
+        set.add("css");
+
+        System.out.println(set);
+        //linkedhashset nao mantem ordem nenhuma
+        Set<String> linkedSet = new LinkedHashSet<>();
+        set.add("java");
+        set.add("php");
+        set.add("php");
+        set.add("c++");
+        set.add("HTML5");
+        set.add("HTML5");
+        set.add("css");
+
+        System.out.println(linkedSet);
     }
 }
