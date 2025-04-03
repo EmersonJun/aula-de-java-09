10f8b1c80274bf69db1195ba012dda6b3cc5562a
Emerson <emersonnjunior2006@gmail.com>
2025-04-03 20:58:43 -0300

    ex1

---
+++ src/ex1/Livro.java
@@ -0,0 +1,50 @@
+package ex1;
+
+import java.util.List;
+
+public class Livro implements Comparable<Livro> {
+    private String nome, autor, genero;
+    private int quantidade;
+    public Livro(String nome, String autor, String genero, int quantidade) {
+        this.nome = nome;
+        this.autor = autor;
+        this.genero = genero;
+        this.quantidade = quantidade;
+    }
+    public void verEstoque(){
+        System.out.println(quantidade);
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    public int getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    @Override
+    public String toString() {
+        return "Livro [nome=" + nome + ", autor=" + autor + ", genero=" + genero + ", quantidade=" + quantidade + "]";
+    }
+    @Override
+    public int compareTo(Livro e) {
+        return 0;
+    }
+    
+}
